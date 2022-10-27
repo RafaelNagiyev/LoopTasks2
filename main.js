44dba4d78ea82c2f05eb14c2efe4913c849744c5
Kerim <86295374+Kerim-tk@users.noreply.github.com>
2022-10-27 13:24:44 +0400

    6

--- main.js
+++ main.js
@@ -61,4 +61,18 @@ function five() {
   }
 }
 
-four();
+function six() {
+  let a = +prompt("Enter A");
+  let b = +prompt("Enter B");
+  while (a != 0 && b != 0) {
+    if (a > b) {
+      a = a % b;
+    } else {
+      b = b % a;
+    }
+
+    console.log(a + b);
+  }
+}
+
+six();
